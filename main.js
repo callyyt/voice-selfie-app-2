888d330c4f97780d76ad1855af1d223e88acd1fc
callyyt <93189620+callyyt@users.noreply.github.com>
2022-06-12 19:00:42 +0530

    Add files via upload
    it is not working for me

---
+++ main.js
@@ -0,0 +1,40 @@
+var speechRecognition=window.webkitSpeechRecognition;
+var recognition= new SpeechRecognition();
+ var Textbox=document.getElementById("textbox");
+
+ function start()
+ {
+     Textbox.innerHTML="";
+     recognition.start();
+ }
+
+ recognition.onresult=function(event)
+ {
+     console.log(event);
+     var Content=event.result[0][0].transcript;
+
+     document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    speak();
+ }
+
+ function speak() {
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").value;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
